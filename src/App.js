52c01770ca5cc9cd8cf7ff808215d6eccb24dd6e
Gaston Franco <gastonfr24@gmail.com>
2022-09-02 20:25:31 -0300

    taiwind-conf

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import './styles/App.css';
+import Home from "containers/pages/Home"; 
+import Error404 from "containers/errores/Error404";
+
+import { Provider } from "react-redux";
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+
 
 function App() {
   return (
-    <div>
-
-    </div>
+    <Provider>
+    <Router>
+    <Routes>
+      {/* Ruta de error */}
+      <Route path='*' element={<Error404/>} />
+    
+      <Route path='/' element={<Home/>} />
+    
+    
+    </Routes>
+    </Router>
+    </Provider>
   );
 }
 
