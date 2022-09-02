61c4b1239d3a58c680ccd5969a5b127be4dfdffe
Gaston Franco <gastonfr24@gmail.com>
2022-09-02 20:26:22 -0300

    tailwind-conf

---
+++ src/containers/errores/Error404.jsx
@@ -0,0 +1,11 @@
+import FullWidthLayout from "hocs/layouts/FullWidthLayout";
+
+function Error404 () {
+    return(
+        <FullWidthLayout>
+            Error 404
+        </FullWidthLayout>
+    )
+}
+
+export default Error404;
---
+++ src/containers/pages/Home.jsx
@@ -0,0 +1,16 @@
+import FullWidthLayout from "hocs/layouts/FullWidthLayout";
+import { connect } from "react-redux";
+
+function Home () {
+    return(
+        <FullWidthLayout>
+            Home
+        </FullWidthLayout>
+    )
+}
+
+const mapStateToProps = state =>({
+
+})
+
+export default connect(mapStateToProps,{})(Home);
