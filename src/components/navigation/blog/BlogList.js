20feaf3c9a7658f657cc5a21e12e5ef293b867eb
Gaston Franco <gastonfr24@gmail.com>
2022-09-03 20:21:37 -0300

    Blog-List-view

--- src/components/navigation/blog/BlogList.js
+++ src/components/navigation/blog/BlogList.js
@@ -1,11 +1,15 @@
 import { useEffect } from "react"
 import { connect } from "react-redux"
 import { get_blog_list, get_blog_list_page } from "redux/actions/blog"
+import LoadingCard from "components/loaders/LoadingCard"
+import BlogCard from "./BlogCard"
+import SmallPaginator from "components/pagination/SmallPaginator"
+
 
 const BlogList=({
     blog_list,
     get_blog_list,
-    get_blog_list_page}) =>{
+    get_blog_list_page,count}) =>{
 
 
     useEffect(() => {
@@ -13,15 +17,38 @@ const BlogList=({
     }, [])
     
     
+    
     return(
         <div>
-            BlogList
+            {
+                blog_list ? 
+                <>
+                <div className="relative bg-gray-50 pb-8 px-4 sm:px-6 lg:pb-12 lg:px-8">
+                    <div className="absolute inset-0">
+                        <div className="bg-white h-1/3 sm:h-2/3" />
+                    </div>
+                    <div className="relative max-w-7xl mx-auto">
+                        
+                        <div className="mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none">
+                            {
+                                blog_list.map(post=>(
+                                    <BlogCard data={post}/>
+                                ))
+                            }
+                        </div>
+                            <SmallPaginator get_blog_list_page={get_blog_list_page} blog_list={blog_list} count={count}/>
+                    </div>
+                </div>
+                </>:
+                <><LoadingCard/></>
+            }
         </div>
     )
 }
 
 const mapToStateProps = state =>({
-    blog_list : state.blog.blog_list
+    blog_list : state.blog.blog_list,
+    cout: state.blog.count
 })
 
 export default connect(mapToStateProps,{
--- src/components/navigation/blog/Header.js
+++ src/components/navigation/blog/Header.js
@@ -1,7 +1,7 @@
 const Header = () =>{
     return(
         <div className="bg-white">
-      <div className="max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
+      <div className="max-w-7xl mx-auto py-8 px-4 sm:py-12 sm:px-6 lg:px-8">
         <div className="text-center">
           <p className="mt-1 text-4xl font-extrabold text-gray-900 sm:text-5xl sm:tracking-tight lg:text-6xl">
             Blog
